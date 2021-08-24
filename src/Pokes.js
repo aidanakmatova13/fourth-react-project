9558b0bfb6a9b3118f007f93060377979854572b
aidanakmatova13 <akmatova.don@gmail.com>
2021-08-24 19:03:37 +0300

    fix pokemon

--- src/Pokes.js
+++ src/Pokes.js
@@ -4,23 +4,34 @@ import axios from "axios";
 
 const Pokes = () => {
     const [pokemon, setPokemon] = useState([])
+    const [search, setSearch] = useState('')
+    const filteredPokemons = pokemon.filter(el => el.name.toLowerCase().includes(search))
 
     useEffect(() =>{
         axios('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
             .then(res => setPokemon(res.data.pokemon))
     },[])
+    const handleSearch =(e)=>{
+        setSearch(e.target.value.toLowerCase())
+    }
 
-    return <div className='row'>
-        {
-            pokemon.map(item  => (
-                    <div className='col-3'>
-                        <img src={item.img} alt="" width='200'/>
-                            <p>{item.name}</p>
-                    </div>
-                )
-            )
-        }
+    return (
+        <div>
+            <input type="text" onChange={handleSearch} className='search'/>
+            <div className='row'>
+                {
+                    filteredPokemons.map(item  => (
+                            <div className='col-3'>
+                                <img src={item.img} alt="" width='200'/>
+                                <p>{item.name}</p>
+                            </div>
+                        )
+                    )
+                }
+            </div>
         </div>
+    )
+
 }
 
 export default Pokes;
